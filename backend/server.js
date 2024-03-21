75669cb764a5bc424dfef9f86d1171773bd69acc
Shubhendra Kumar <123719235+shubhkeshari48@users.noreply.github.com>
2024-03-21 13:39:31 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
@@ -6,13 +7,19 @@ import userRoutes from './routes/userRoutes.js'
 import cookieParser from 'cookie-parser';
 import connectToMongo from './db/conn.js';
 import { app,server } from './socket/socket.js';
+
 dotenv.config();
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT || 8000
+const __dirname=path.resolve();
 app.use(express.json())
 app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes)
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
     connectToMongo();
